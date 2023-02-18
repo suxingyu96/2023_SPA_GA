2d45c7493735480ff2bebe718d7a6f179ecec512
suxingyu96 <sugefey@gmail.com>
2023-02-18 16:11:57 +0000

    Feb 21

---
+++ src/objects/student.py
@@ -0,0 +1,16 @@
+class Student:
+
+    def __init__(self, name, stuID, projectList: list, project_id=None):
+        self.__name = name
+        self.__stuID = stuID
+        self.__projectList = projectList
+        self.__project_id = None
+
+    def getStudentName(self):
+        return self.__name
+
+    def getStudentID(self):
+        return self.__stuID
+
+    def getProjectList(self):
+        return self.__projectList
