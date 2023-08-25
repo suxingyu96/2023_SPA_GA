fe8a91a67465927fbdc8eb7fb782a7096cf9de18
suxingyu96 <sugefey@gmail.com>
2023-08-25 22:49:45 +0100

    Aug 25 - added setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='23_SPA_GA_Implementation',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='https://github.com/suxingyu96/2023_SPA_GA',
+    license='',
+    author='suxingyu',
+    author_email='sugefey@gmail.com',
+    description=''
+)
